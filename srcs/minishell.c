41ea9433e109fae4b4aeb2cd9279fa8094702562
Gonzalo Liano ricoy <goliano-@c1r17s1.42madrid.com>
2022-02-09 18:40:52 +0100

    readline bug Ctrl D segfault

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,11 +6,23 @@
 /*   By: goliano- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 11:33:37 by goliano-          #+#    #+#             */
-/*   Updated: 2022/02/09 15:24:02 by ajimenez         ###   ########.fr       */
+/*   Updated: 2022/02/09 18:40:20 by goliano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+/*
+ * SIGINT CTRL + C == 2
+ *
+ *
+ *
+ */
+
+void handle_sigint(int n)
+{
+	printf("SEÑAL: %d\n", n);
+}
 
 int main(int argc, char **argv)
 {
@@ -23,16 +35,16 @@ int main(int argc, char **argv)
 	init_prompt(&g_data, environ);
 	//execve("/usr/bin/whoami", cmd, environ);
 	//execve("/usr/bin/hostnamectl", cmd, environ);
+	signal(SIGQUIT, handle_sigint);
 	while (42)
 	{
 		inp = readline(g_data.prompt);
 		//printf("C: %c\n", inp[0]);
 		//return 1;
+		if (!inp)
+			return (1);
 		if (inp[0])
-		{
-			//printf("-----DEBUGGER00----\n");
 			add_history(inp);
-		}
 		//lexer(inp);
 		printf("INP: %s\n", inp);
 	}
