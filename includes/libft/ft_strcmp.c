cce1280461c98e6e485a357b2117f98f3e63f170
Alex <jimenezs.alex@gmail.com>
2022-06-08 14:07:23 +0200

    new libft function, ft_strcmp

--- includes/libft/ft_strcmp.c
+++ includes/libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 11:16:19 by ajimenez          #+#    #+#             */
-/*   Updated: 2022/06/07 13:00:02 by ajimenez         ###   ########.fr       */
+/*   Updated: 2022/06/08 13:57:55 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@ int	ft_strcmp(const char *src, const char *to_cmp)
 	size_t	n;
 
 	i = 0;
-	n = ft_strlen(s2);
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+	n = ft_strlen(src);
+	while (src[i] != '\0' && to_cmp[i] != '\0' && i < n)
 	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		if (src[i] != to_cmp[i])
+			return ((unsigned char)src[i] - (unsigned char)src[i]);
 		++i;
 	}
 	if (i != n)
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		return ((unsigned char)src[i] - (unsigned char)to_cmp[i]);
 	return (0);
 }
