8ebbaa70d5fc8d09f44f1a66f39289977f64c933
Alex <jimenezs.alex@gmail.com>
2022-06-15 12:34:25 +0200

    tmp

--- srcs/builtins/ft_export.c
+++ srcs/builtins/ft_export.c
@@ -6,15 +6,38 @@
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 14:41:24 by ajimenez          #+#    #+#             */
-/*   Updated: 2022/06/14 14:02:48 by ajimenez         ###   ########.fr       */
+/*   Updated: 2022/06/15 10:54:19 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static _Bool	env_exist(char **cmd, t_env *env)
+static size_t	len_to_token(char *s, char c)
 {
-	if (ft_strncmp(cmd[1], const char *s2, size_t n))
+	int	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
+}
+
+static t_dlist	*env_exist(char **cmd, t_dlist *env)
+{
+	while (env)
+	{
+		if (ft_strncmp(cmd[1], (char *)env->content,
+			len_to_token((char *)env->content, '=')))
+			return (env);
+		env = env->next;
+	}	
+	return (NULL);
+}
+
+static void	replace_var(t_dlist **env, char *new_var)
+{
+	free((*env)->content);
+	(*env)->content = ft_dlstnew(new_var);
 }
 
 int ft_export(t_env *env, char **cmd)	
@@ -25,6 +48,5 @@ int ft_export(t_env *env, char **cmd)
 	if (matrix_data.line_count == 0)
 		ft_putstr_fd("\n", 1);
 	else
-		//TODO -> funcion que busque y agregue al final dell nodo
-		f
+		env_exist(cmd, (t_dlist *)env->env_lst);
 }
