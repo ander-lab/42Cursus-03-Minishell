5804c466a54aad3cab1a22c68d04b7beffce3287
Gonzalo <gonzalowtc@gmail.com>
2022-09-04 23:12:05 +0200

    leaks

--- utils/free/free.c
+++ utils/free/free.c
@@ -16,10 +16,8 @@ void	free_t_lst(t_list *lst)
 {
 	char	*key;
 	char	*value;
-	int	i;
 	t_list	*aux;
 
-	i = 0;
 	while (lst)
 	{
  		key = ((t_env_line *)(lst)->content)->key;
@@ -30,7 +28,6 @@ void	free_t_lst(t_list *lst)
 		aux = lst;
 		lst = lst->next;
 		free(aux);
-		i++;
 	}
 	free(lst);
 }
@@ -42,25 +39,26 @@ void	free_t_cmds(t_cmds *cmds)
 	while (cmds)
 	{
 		tmp = cmds;
-		free(cmds->content);
+		free(tmp->content);
 		cmds = cmds->next;
 		free(tmp);
 	}
-	free(cmds);
+	//free(cmds);
 }
 
 static void	free_t_dlst(t_dlist *lst)
 {
-	t_dlist	*aux;
+	t_dlist		*aux;
 
 	while (lst)
 	{
 		aux = lst;
-		free(((t_token_data *)(lst)->content)->str);
-		free(lst->content);
+		free(((t_token_data *)(aux)->content)->str);
+		free(aux->content);
 		lst = lst->next;
 		free(aux);
 	}
+	//free(lst);
 }
 
 void	free_gdata(t_gdata *gdata)
@@ -69,9 +67,14 @@ void	free_gdata(t_gdata *gdata)
 	free_t_lst(gdata->env->env_lst);
 	free(gdata->prompt);
 	free(gdata->env);
-	free_t_cmds(gdata->cmds_lst);
-	ft_free_matrix(gdata->cmds);
-	free_t_dlst(gdata->glob_lst);
-	free(gdata->heredoc);
-	free(gdata->fd);
+	if (gdata->cmds_lst)
+		free_t_cmds(gdata->cmds_lst);
+	if (gdata->cmds)
+		ft_free_matrix(gdata->cmds);
+	if (gdata->glob_lst)
+		free_t_dlst(gdata->glob_lst);
+	if (gdata->heredoc)
+		free(gdata->heredoc);
+	if (gdata->fd)
+		free(gdata->fd);
 }
